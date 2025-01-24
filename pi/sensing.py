c73b97b77a8a3507e45283492ed474920ca1f29a
Zakattack1045 <64319398+Zakattack1045@users.noreply.github.com>
2025-01-24 23:07:43 +0000

    Add files via upload
    Added basic conversion to gravitational constant units, adjusting on-chip filtering.

--- pi/sensing.py
+++ pi/sensing.py
@@ -1,3 +1,4 @@
+
 import smbus2, time
 
 LIS3DH_ADDRESS = 0x18
@@ -6,7 +7,7 @@ LIS3DH_ADDRESS = 0x18
 LIS3DH_REG_CTRL_REG1 = 0x20
 LIS3DH_REG_CTRL_REG2 = 0x21
 LIS3DH_REG_CTRL_REG3 = 0x22
-LIS3DH_REG_CTRL_REG4 = 0x23
+LIS3DH_REG_CTRL_REG4 = 0x23 #Set scale with this (page 37 of MEMS data sheet) default is +/- 2g
 LIS3DH_REG_CTRL_REG5 = 0x24
 LIS3DH_REG_CTRL_REG6 = 0x25
 
@@ -34,9 +35,9 @@ def lis3dh_init() -> None:
     print('Initialising LIS3DH...')
     # enable high resolution, xyz axes, and 100Hz sampling
     i2c_write_reg(LIS3DH_ADDRESS, LIS3DH_REG_CTRL_REG1, 0b0101_0111)
-
+    #High resolution means each digit represets 1mg
     # set HPF
-    i2c_write_reg(LIS3DH_ADDRESS, LIS3DH_REG_CTRL_REG2, 0b0000_0000)
+    i2c_write_reg(LIS3DH_ADDRESS, LIS3DH_REG_CTRL_REG2, 0b0000_0010)
 
     # the rest of the registers are ok with 0x00 by default
 
@@ -62,7 +63,7 @@ def lis3dh_read_xyz_raw() -> tuple[int, int, int]:
     return (x, y, z)
 
 
-def lis3dh_read_xyz() -> tuple[float, float, float]:
+def lis3dh_read_xyz() -> tuple[float, float, float]:#Changing from two's complement
     x, y, z = lis3dh_read_xyz_raw()
 
     if x & 0x8000:
@@ -72,8 +73,13 @@ def lis3dh_read_xyz() -> tuple[float, float, float]:
     if z & 0x8000:
         z -= 0x10000
     
-    return (x, y, z)
+    #Comment this section out if you want raw data
+    max_value = 2 #Since scale is +/- 2g by default
+    x = x/32768 * max_value
+    y = y/32768 * max_value
+    z = z/32768 * max_value
 
+    return (x, y, z)
 
 if __name__ == "__main__":
     lis3dh_init()
