bb67117888a4b20e107c2ea513e4b059a9b3ec1f
ogahector <ogahector@gmail.com>
2025-01-27 23:08:27 +0000

    distance from calibration almost done, data formatting rules

--- pi/rep_analysis.py
+++ pi/rep_analysis.py
@@ -7,9 +7,25 @@ from dataclasses import dataclass
 from enum import Enum
 import numpy as np
 
+import matplotlib.pyplot as plt
 
 # import magnet as mag
 # import accelerometer
+
+"""
+NOTE:
+For this, the data will be packaged as such:
+all points correspond to an entry in the list:
+
+eg. SetData.accel = [   
+                        [x0, y0, z0],
+                        [x1, y1, z2],
+                        [x2, y2, z2],
+                        ...
+                        ...
+                    ]
+
+"""
 
 ## CLASSES
 @dataclass
@@ -25,19 +41,34 @@ class Exercise(Enum):
 
 
 ## FNS
-def distance_pnt2pnt(x1, y1, z1, x2, y2, z2) -> float:
-    return np.sqrt( (x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2 )
-
 def distance_pnt2pnt(p1, p2) -> float:
     return np.sqrt( (p1[0] - p2[0])**2 + (p1[1] - p1[1])**2 + (p1[2] - p2[2])**2 )
 
 
+def contains_nan(dataset) -> bool:
+    dataset = np.array(dataset)
+    dataset.flatten()
+    # print(dataset)
+    for i in dataset:
+        if i is np.nan or i == np.nan: 
+            return True
+    return False
+
+
+def isolate_axis(points: list[list[float]], axis: int) -> list[float]:
+    return [point[axis] for point in points]
+
+
 def closest_point_on_line(point: list[float], line: list[list[float]]):
     min_dist = np.inf
+    closest_point = None
     for linept in line:
-        min_dist = min_dist if min_dist < distance_pnt2pnt(point, linept) else distance_pnt2pnt(point, linept)
+        dist = distance_pnt2pnt(point, linept)
+        if dist < min_dist:
+            min_dist = dist
+            closest_point = linept
 
-    return min_dist
+    return closest_point, min_dist
     
 
     # Use linear algebra to find closest point
@@ -57,7 +88,7 @@ def closest_point_on_line(point: list[float], line: list[list[float]]):
         # Project AP onto AB, get the parameter t
         t = np.dot(AP, AB) / np.dot(AB, AB)
 
-        print(t)
+        # print(t)
         
         # Clamp t to the range [0, 1]
         t = max(0, min(1 , t))
@@ -73,14 +104,14 @@ def closest_point_on_line(point: list[float], line: list[list[float]]):
             min_distance = distance
             closest_point = Q
     
-    return list(closest_point), min_distance
+    return closest_point, min_distance
 
 
 def get_SetData_pos_vel(data: SetData) -> list[ list[list] ]:
     sample_time = data.sample_time
 
-    pos = [(0, 0, 0)]
-    vel = [(0, 0, 0)]
+    pos = [(0.0, 0.0, 0.0)]
+    vel = [(0.0, 0.0, 0.0)]
 
     current_x_vel = 0
     current_y_vel = 0
@@ -95,19 +126,21 @@ def get_SetData_pos_vel(data: SetData) -> list[ list[list] ]:
         current_y_vel += accel[1] * sample_time
         current_z_vel += accel[2] * sample_time
 
-        vel += (current_x_vel, current_y_vel, current_z_vel)
+        vel.append((current_x_vel, current_y_vel, current_z_vel))
 
     for v in vel:
-        current_x_pos = v[0] * sample_time
-        current_y_pos = v[1] * sample_time
-        current_z_pos = v[2] * sample_time
+        current_x_pos += v[0] * sample_time
+        current_y_pos += v[1] * sample_time
+        current_z_pos += v[2] * sample_time
 
         pos.append((current_x_pos, current_y_pos, current_z_pos))
 
-    print(pos)
-    print(vel)
+    # print(len(pos))
+    # print(len(vel))
 
-    return pos, vel
+    return pos[1:], vel[1:]
+
+
 
 
 def analyse_set(data: SetData, exercise: Exercise, cal_rep: SetData):
@@ -141,34 +174,53 @@ def analyse_set(data: SetData, exercise: Exercise, cal_rep: SetData):
             print('Choose a Valid Exercise!')
             return
 
-    print(cal_pos)
+    # print(f'set_pos: {set_pos}')
 
     dists_to_cal: list[float] = []
     for point in set_pos:
-        print(point)
+        # print(point)
         closest_point, dist_to_closest = closest_point_on_line(point, cal_pos)
-        dists_to_cal += dist_to_closest
+        dists_to_cal.append(dist_to_closest)
 
-    dists_sd = np.var(dists_to_cal)
+    dists_var = np.var(dists_to_cal)
+
+    if(contains_nan(dists_to_cal)):
+        print('CONTAINS NANSS!!')
 
     # do sth with it
 
-    return dists_sd
+    return dists_to_cal, dists_var
 
 
 def main() -> None:
-    t = np.arange(0, 10, 0.01)
-    perfect = np.cos(t), np.zeros(len(t)), np.zeros(len(t))
-    noise = np.random.random(len(t)), np.zeros(len(t)), np.zeros(len(t))
+    ts = 0.01
+    t = np.arange(0, 10, ts)
 
-    calibration = SetData(perfect, 0, 0.01)
+    perfect = np.array([(np.cos(i), 0, 0) for i in t])
+    noise = np.array([( (np.random.random() - 0.5), 0, 0) for _ in t])
 
-    set = SetData(0, 0, 0.01)
-    set.accel = (perfect + noise) * 3
+    zeros_3d = [(0,0,0) for _ in t]
 
-    print(set.accel)
+    calibration = SetData(perfect, zeros_3d, 0.01)
 
-    print(analyse_set(set, Exercise.TRICEP_PULLDOWN, calibration))
+    set = SetData(None,  zeros_3d, ts)
+    # set.accel = (perfect[0] + noise[0] - 0.5, np.zeros(len(t)), np.zeros(len(t)))
+    set.accel = [perfect[i] * 3 + noise[i] for i in range( len(t) )]
+
+    # print(len(set.accel[0]))
+    
+    min_dists, _ = analyse_set(set, Exercise.TRICEP_PULLDOWN, calibration)
+
+
+    print(min_dists)
+
+    plt.figure()
+    plt.plot(t, isolate_axis(perfect, 0), '--r')
+    plt.plot(t, isolate_axis(set.accel, 0), '-b')
+    plt.plot(t, min_dists[1:], '-k')
+    plt.legend(["Calibration Rep", "Our Rep", "Min Dists"])
+    plt.grid(True)
+    plt.show()
 
 
 if __name__ == '__main__':
