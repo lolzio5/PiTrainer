532a351b954af0ea4b2cc5be1c590ed47c0d1192
lolzio5 <loleziovioramarquet@gmail.com>
2025-02-12 12:05:54 +0000

    edited margins

--- app/(tabs)/workout.tsx
+++ app/(tabs)/workout.tsx
@@ -184,7 +184,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    marginBottom: 10,
+    marginBottom: 5,
   },
   exerciseImage: {
     width: 300,
